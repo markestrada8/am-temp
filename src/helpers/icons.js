67ed8f6dc6e3131374e38c4fb702e71c6dde4fbd
Mark Estrada <mark.estrada.dev>
2024-12-21 00:10:12 -0600

    update

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,6 +1,7 @@
-import { 
-    faTrash, 
-    faSignOutAlt, 
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+    faTrash,
+    faSignOutAlt,
     faEdit,
     faSpinner,
     faPlusCircle,
@@ -8,22 +9,22 @@ import {
     faEnvelope,
     faMapMarkedAlt,
     faLock
-} from "@fortawesome/free-solid-svg-icons";
+} from "@fortawesome/free-solid-svg-icons"
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 
 const Icons = () => {
     return library.add(
-        faTrash, 
-        faSignOutAlt, 
-        faEdit, 
-        faSpinner, 
-        faPlusCircle, 
+        faTrash,
+        faSignOutAlt,
+        faEdit,
+        faSpinner,
+        faPlusCircle,
         faPhone,
         faEnvelope,
         faMapMarkedAlt,
         faLock
-    );
-};
+    )
+}
 
-export default Icons;
+export default Icons
